eeeeb606005679d5899a9df9b01e98e700c65fff
Yeliz Erdogan <yelizerdogan1988@gmail.com>
2023-01-03 00:42:13 +0300

    03.01.22

--- src/test/java/Appium/trendyol.java
+++ src/test/java/Appium/trendyol.java
@@ -1,8 +1,12 @@
 package Appium;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.TapOptions;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -23,7 +27,7 @@ public class trendyol {
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\USERR\\IdeaProjects\\AppiumFirst\\src\\Apps\\Trendyol.apk");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\USERR\\IdeaProjects\\AppiumFirst\\src\\Apps\\Trendyol.apk");
 
         capabilities.setCapability("noReset", true);
 
@@ -32,5 +36,18 @@ public class trendyol {
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(driver.findElementByXPath("//android.widget.TextView[@text='Trendyol Go']").isDisplayed());
+        System.out.println("trenyol uygulamasindayiz");
+
+        MobileElement textBox1 = driver.findElementByClassName("android.widget.EditText");
+        textBox1.click();
+        Thread.sleep(3000);
+        MobileElement textBox2 = driver.findElementById("trendyol.com:id/edittext_search_view");
+        textBox2.sendKeys("kalem");
+        Thread.sleep(3000);
+
+
+
     }
 }
